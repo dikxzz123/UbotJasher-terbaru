4c545a714568067d895b5caf56bfbf76250f9162
DIKXZ OFFICIAL <kaandik826@gmail.com>
2025-09-24 21:38:49 +0700

    Upload 13 item(s) via Uploader Pro

---
+++ setting.js
@@ -0,0 +1,22 @@
+// setting.js
+
+
+/*⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞
+  ⏤͟͟͞͞⚡ SCRIPT BOT BY JUN OFFICIAL ⚡⏤͟͟͞͞
+⏤͟͟͞͞ Dibuat bukan hanya untuk jalan, tapi untuk menang
+⏤͟͟͞͞ Follow Channel: @Junofficial354ch2
+⏤͟͟͞͞ Telegram Bot : @Junofficial354new
+⏤͟͟͞͞ Tanks Buat pengguna script ini ✰
+⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞
+*/
+module.exports = {
+  TOKEN: '8435962401:AAGCCrUaKGHAibte2d07Io_-tLwpwYnmIiU',
+  OWNER_ID: 7739215270, // owner 
+  GROUP : -1002741171888, // gc 
+  CHANEL : -1003080046052, // chanel lu yaa
+  gclog : -1002959113773, // group buat share all log bot/ history
+  MAX_LIMIT: 100,
+  TokenVercel : "P6JQ6CrS37J1ONjxlqN9Esf8",
+  MENU_IMAGE_URL: 'https://ar-hosting.pages.dev/1758181333367.jpg', // link foto menu
+};
+
